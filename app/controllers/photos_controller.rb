72c2e36c2f3bc47a3c62aae5c1b31817a405bcb8
xttebrocx <samueldcorbett@uchicago.edu>
2022-05-06 20:58:57 +0000

    All main links working, now have to create forms

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,14 @@
 class PhotosController < ApplicationController
   def index
+    photos = Photo.all
+    @list_of_photos = photos.order(:created_at=>:desc)
     render({:template => "photos/index.html.erb"})
+  end
+
+  def show
+    @path = params.fetch(:photo_path)
+    @photo =Photo.where(:id=>@path).at(0)
+
+    render({:template=>"photos/view.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get("/photos", :controller =>"photos", :action=>"index")
 
   get("/users/:username", :controller => "users", :action =>"show")
+  
+  get("/photos/:photo_path", :controller => "photos", :action =>"show")
 
   
 end
